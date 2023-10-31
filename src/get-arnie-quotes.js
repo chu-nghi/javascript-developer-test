87f581d1037df97fa65685527b98be1b86aeb887
Kevin Chu <chuchieunghi@gmail.com>
2023-10-31 22:24:05 +1100

    fix: Implement function  getArnieQuotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,9 +1,29 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const funcArr = [];
+  for (let url of urls) {
+    funcArr.push(httpGet(url));
+  }
+  const promiseResponses = await Promise.all(funcArr);
+  return processPromise(promiseResponses);
 };
+
+const processPromise = (promiseResponses) => {
+  const results = [];
+  for (let promiseRes of promiseResponses) {
+    if (promiseRes.status === 200) {
+      results.push({
+        'Arnie Quote': JSON.parse(promiseRes.body)?.message
+      })
+    } else {
+      results.push({
+        'FAILURE': JSON.parse(promiseRes.body)?.message
+      })
+    }
+  }
+  return results;
+}
 
 module.exports = {
   getArnieQuotes,
